5e52841fece6cfc3c7d96d08de4535f1e0ebca74
maximilian.pallasch <maximilian.pallasch@study.hs-duesseldorf.de>
2021-08-17 14:49:09 +0200

    show water when opening tap

---
+++ Assets/TapWater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapWater : MonoBehaviour
+{
+    public GameObject rotatingSphere;
+
+    private MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(rotatingSphere.transform.localEulerAngles.x);
+        if (rotatingSphere.transform.localEulerAngles.x > 22.5f)
+        {
+            meshRenderer.enabled = true;
+        } else
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+}
