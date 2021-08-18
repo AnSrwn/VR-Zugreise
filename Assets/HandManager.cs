b01df76053fe6d9c782ae67761c6752c633da7de
maximilian.pallasch <maximilian.pallasch@study.hs-duesseldorf.de>
2021-08-18 16:11:31 +0200

    washing hands changes gloves

---
+++ Assets/HandManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class HandManager : MonoBehaviour
+{
+    public GameObject leftRenderModel;
+    public GameObject rightRenderModel;
+    public GameObject leftRenderModelSlim;
+    public GameObject rightRenderModelSlim;
+
+    public void SetRenderModel(GameObject model, bool right)
+    {
+        for (int handIndex = 0; handIndex < Player.instance.hands.Length; handIndex++)
+        {
+            Hand hand = Player.instance.hands[handIndex];
+            if (hand != null)
+            {
+                if (hand.handType == SteamVR_Input_Sources.RightHand && right)
+                    hand.SetRenderModel(model);
+                if (hand.handType == SteamVR_Input_Sources.LeftHand && !right)
+                    hand.SetRenderModel(model);
+            }
+        }
+    }
+}
--- Assets/TapWater.cs
+++ Assets/TapWater.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class TapWater : MonoBehaviour
 {
     public GameObject rotatingSphere;
+    public HandManager handManager;
 
     private MeshRenderer meshRenderer;
 
@@ -17,13 +19,30 @@ public class TapWater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(rotatingSphere.transform.localEulerAngles.x);
         if (rotatingSphere.transform.localEulerAngles.x > 22.5f)
         {
             meshRenderer.enabled = true;
         } else
         {
             meshRenderer.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(meshRenderer.enabled)
+        {
+            name = other.gameObject.name;
+            Match matchRight = Regex.Match(name, "^finger.*r$");
+            Match matchLeft = Regex.Match(name, "^finger.*l$");
+            if (matchRight.Success)
+            {
+                handManager.SetRenderModel(handManager.rightRenderModel, true);
+            }
+            else if(matchLeft.Success)
+            {
+                handManager.SetRenderModel(handManager.leftRenderModel, false);
+            }
         }
     }
 }
